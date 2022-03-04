3a0706b218caa1a1ff9a9035858cd86a7bcc5c41
Jethro Williams <jethrowilliams@outlook.com>
2022-03-04 18:02:28 +0000

    Add first intro animations

--- components/homepage-sections/introduction/Introduction.js
+++ components/homepage-sections/introduction/Introduction.js
@@ -5,11 +5,14 @@ const Introduction = () => {
   return (
     <SectionContainer className='bg-black'>
       <div className='flex w-full h-full'>
-        <div className='flex flex-col items-center basis-1/2'>
-          <Title className='text-white'>Jethro Williams</Title>
-          <Title className='text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-fuchsia-500'>
+        <div className='flex flex-col items-center basis-1/2 gap-5 pt-20'>
+          <Title className='text-white animate-zoom-down-in'>Jethro Williams</Title>
+          <Title className='text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-fuchsia-500 animate-zoom-up-in-delayed'>
             Software Engineer
           </Title>
+          <div className='text-slate-600 text-xl uppercase text-center animate-slide-fade-up-delayed'>
+            This is some really inspirational text about some thing or whatever.
+          </div>
         </div>
         <div className='basis-1/2 h-inherit'>
           <img
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,41 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        'slide-in-right': 'slideInRight 0.4s ease-out forwards',
+        'slide-fade-up-delayed': 'slideFadeUpDelayed 2s',
+        'slide-in-right': 'slideInRight 1s ease-out forwards',
+        'zoom-down-in': 'zoomDownIn 1s',
+        'zoom-up-in-delayed': 'zoomUpInDelayed 2s',
       },
       keyframes: {
+        slideFadeUpDelayed: {
+          '0%': { transform: 'translateY(25vh)', opacity: 0 },
+          '20%': { transform: 'translateY(25vh)', opacity: 0 },
+          '100%': { transform: 'translateY(0)', opacity: 1 },
+        },
         slideInRight: {
           '0%': { transform: 'translateX(-100vw)' },
           '100%': { transform: 'translateX(0)' },
+        },
+        zoomDownIn: {
+          '0%': {
+            transform: 'perspective(500px) translateZ(-500px) translateY(-50px)',
+            opacity: 0,
+          },
+          '100%': { transform: 'perspective(500px) translateZ(0) translateY(0)', opacity: 1 },
+        },
+        zoomUpInDelayed: {
+          '0%': {
+            transform: 'perspective(500px) translateZ(-500px) translateY(50px)',
+            opacity: 0,
+          },
+          '20%': {
+            transform: 'perspective(500px) translateZ(-500px) translateY(50px)',
+            opacity: 0,
+          },
+          '100%': {
+            transform: 'perspective(500px) translateZ(0) translateY(0)',
+            opacity: 1,
+          },
         },
       },
       minHeight: {
