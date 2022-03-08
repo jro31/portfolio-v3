e578caf961c734556f05eef05962f83d7e1f511e
Jethro Williams <jethrowilliams@outlook.com>
2022-03-08 10:33:05 +0000

    Remove scroll snap on mobile

--- components/homepage-sections/SectionContainer.js
+++ components/homepage-sections/SectionContainer.js
@@ -1,6 +1,6 @@
 const SectionContainer = props => {
   return (
-    <div className={`h-screen box-border snap-start ${props.className || ''}`}>
+    <div className={`h-screen box-border lg:snap-start ${props.className || ''}`}>
       <div className='pt-navbar-height h-full w-full'>{props.children}</div>
     </div>
   );
