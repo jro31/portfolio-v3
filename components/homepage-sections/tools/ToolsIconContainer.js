97284aae28cef0cecd27a475bd3c12a0a42f3d38
Jethro Williams <jethrowilliams@outlook.com>
2022-03-14 16:50:24 +0000

    Tools icons refactor and highlight the logo on hover

--- components/homepage-sections/tools/ToolsIconContainer.js
+++ components/homepage-sections/tools/ToolsIconContainer.js
@@ -1,8 +1,135 @@
+import { useState } from 'react';
+import BitbucketIcon from '../../ui/svg/BitBucketIcon';
+import BootstrapIcon from '../../ui/svg/BootstrapIcon';
+import CloudinaryIcon from '../../ui/svg/CloudinaryIcon';
+import CssIcon from '../../ui/svg/CssIcon';
+import GitHubIcon from '../../ui/svg/GitHubIcon';
+import GitIcon from '../../ui/svg/GitIcon';
+import HamlIcon from '../../ui/svg/HamlIcon';
+import HerokuIcon from '../../ui/svg/HerokuIcon';
+import HtmlIcon from '../../ui/svg/HtmlIcon';
+import JavascriptIcon from '../../ui/svg/JavascriptIcon';
+import JQueryIcon from '../../ui/svg/JQueryIcon';
+import NetlifyIcon from '../../ui/svg/NetlifyIcon';
+import NextJsIcon from '../../ui/svg/NextJsIcon';
+import PostgreSqlIcon from '../../ui/svg/PostgreSqlIcon';
+import RailsIcon from '../../ui/svg/RailsIcon';
+import ReactIcon from '../../ui/svg/ReactIcon';
+import ReduxToolkitIcon from '../../ui/svg/ReduxToolkitIcon';
+import RSpecIcon from '../../ui/svg/RSpecIcon';
+import RubyIcon from '../../ui/svg/RubyIcon';
+import S3Icon from '../../ui/svg/S3Icon';
+import SassIcon from '../../ui/svg/SassIcon';
+import SublimeIcon from '../../ui/svg/SublimeIcon';
+import TailwindIcon from '../../ui/svg/TailwindIcon';
+import VercelIcon from '../../ui/svg/VercelIcon';
+import VSCodeIcon from '../../ui/svg/VSCodeIcon';
+
+import {
+  bitbucket,
+  bootstrap,
+  cloudinary,
+  css,
+  git,
+  gitHub,
+  haml,
+  heroku,
+  html,
+  javaScript,
+  jQuery,
+  netlify,
+  nextJS,
+  postgreSQL,
+  reactString,
+  reduxToolkit,
+  rSpec,
+  ruby,
+  rubyOnRails,
+  s3,
+  sass,
+  sublimeText,
+  tailwindCSSString,
+  vercel,
+  vSCode,
+} from './ToolsIconsSection';
+
 const ToolsIconContainer = props => {
+  const defaultIconColor = 'rgb(71 85 105)';
+  const [iconColor, setIconColor] = useState(defaultIconColor);
+
+  const iconComponent = () => {
+    switch (props.iconName) {
+      case ruby:
+        return <RubyIcon color={iconColor} />;
+      case javaScript:
+        return <JavascriptIcon color={iconColor} />;
+      case html:
+        return <HtmlIcon color={iconColor} />;
+      case css:
+        return <CssIcon color={iconColor} />;
+      case haml:
+        return <HamlIcon color={iconColor} />;
+      case jQuery:
+        return <JQueryIcon color={iconColor} />;
+      case rubyOnRails:
+        return <RailsIcon color={iconColor} />;
+      case reactString:
+        return <ReactIcon color={iconColor} />;
+      case nextJS:
+        return <NextJsIcon color={iconColor} />;
+      case git:
+        return <GitIcon color={iconColor} />;
+      case gitHub:
+        return <GitHubIcon color={iconColor} />;
+      case bitbucket:
+        return <BitbucketIcon color={iconColor} />;
+      case tailwindCSSString:
+        return <TailwindIcon color={iconColor} />;
+      case bootstrap:
+        return <BootstrapIcon color={iconColor} />;
+      case sass:
+        return <SassIcon color={iconColor} />;
+      case heroku:
+        return <HerokuIcon color={iconColor} />;
+      case vercel:
+        return <VercelIcon color={iconColor} />;
+      case netlify:
+        return <NetlifyIcon color={iconColor} />;
+      case s3:
+        return <S3Icon color={iconColor} />;
+      case cloudinary:
+        return <CloudinaryIcon color={iconColor} />;
+      case postgreSQL:
+        return <PostgreSqlIcon color={iconColor} />;
+      case vSCode:
+        return <VSCodeIcon color={iconColor} />;
+      case sublimeText:
+        return <SublimeIcon color={iconColor} />;
+      case rSpec:
+        return <RSpecIcon color={iconColor} />;
+      case reduxToolkit:
+        return <ReduxToolkitIcon color={iconColor} />;
+      default:
+        throw new Error(`Unrecognised icon name '${props.iconName}' passed to ToolsIconsSection`);
+    }
+  };
+
+  const mouseOverHandler = () => {
+    setIconColor('white');
+  };
+
+  const mouseOutHandler = () => {
+    setIconColor(defaultIconColor);
+  };
+
   return (
-    <div className='flex flex-col gap-3 text-slate-600 hover:text-white h-40'>
-      <div className='h-20 w-20'>{props.children}</div>
-      <div className='text-center'>{props.name}</div>
+    <div
+      onMouseOver={mouseOverHandler}
+      onMouseOut={mouseOutHandler}
+      className='flex flex-col gap-3 text-slate-600 hover:text-white h-40'
+    >
+      <div className='h-20 w-20'>{iconComponent()}</div>
+      <div className='text-center'>{props.iconName}</div>
     </div>
   );
 };
--- components/homepage-sections/tools/ToolsIcons.js
+++ components/homepage-sections/tools/ToolsIcons.js
@@ -1,143 +1,32 @@
-import BitbucketIcon from '../../ui/svg/BitBucketIcon';
-import BootstrapIcon from '../../ui/svg/BootstrapIcon';
-import CloudinaryIcon from '../../ui/svg/CloudinaryIcon';
-import CssIcon from '../../ui/svg/CssIcon';
-import GitHubIcon from '../../ui/svg/GitHubIcon';
-import GitIcon from '../../ui/svg/GitIcon';
-import HamlIcon from '../../ui/svg/HamlIcon';
-import HerokuIcon from '../../ui/svg/HerokuIcon';
-import HtmlIcon from '../../ui/svg/HtmlIcon';
-import JavascriptIcon from '../../ui/svg/JavascriptIcon';
-import JQueryIcon from '../../ui/svg/JQueryIcon';
-import NetlifyIcon from '../../ui/svg/NetlifyIcon';
-import NextJsIcon from '../../ui/svg/NextJsIcon';
-import PostgreSqlIcon from '../../ui/svg/PostgreSqlIcon';
-import RailsIcon from '../../ui/svg/RailsIcon';
-import ReactIcon from '../../ui/svg/ReactIcon';
-import ReduxToolkitIcon from '../../ui/svg/ReduxToolkitIcon';
-import RSpecIcon from '../../ui/svg/RSpecIcon';
-import RubyIcon from '../../ui/svg/RubyIcon';
-import S3Icon from '../../ui/svg/S3Icon';
-import SassIcon from '../../ui/svg/SassIcon';
-import SublimeIcon from '../../ui/svg/SublimeIcon';
-import TailwindIcon from '../../ui/svg/TailwindIcon';
-import VercelIcon from '../../ui/svg/VercelIcon';
-import VSCodeIcon from '../../ui/svg/VSCodeIcon';
-import ToolsIconContainer from './ToolsIconContainer';
 import ToolsIconsSection from './ToolsIconsSection';
 
-const ToolsIcons = () => {
-  const iconColor = 'rgb(71 85 105)';
+export const languages = 'Languages';
+export const librariesAndFrameworks = 'Libraries/Frameworks';
+export const versionControl = 'Version control';
+export const styling = 'Styling';
+export const hosting = 'Hosting';
+export const fileStorage = 'File storage';
+export const dataString = 'Data';
+export const coding = 'Coding';
+export const testing = 'Testing';
+export const stateManagement = 'State management';
 
+const ToolsIcons = () => {
   return (
     <div className='overflow-x-scroll w-full'>
       <div className='flex gap-5 w-full'>
         {/* TODO - Can you make each SVG a different color, and transition them periodically to other colors? */}
         {/* Probably give the container a class and add a wildcard transition to all child elements in the global css */}
-        <ToolsIconsSection name='Languages'>
-          <ToolsIconContainer name='Ruby'>
-            <RubyIcon color={iconColor} />
-          </ToolsIconContainer>
-          <ToolsIconContainer name='JavaScript'>
-            <JavascriptIcon color={iconColor} />
-          </ToolsIconContainer>
-          <ToolsIconContainer name='HTML'>
-            <HtmlIcon color={iconColor} />
-          </ToolsIconContainer>
-          <ToolsIconContainer name='CSS'>
-            <CssIcon color={iconColor} />
-          </ToolsIconContainer>
-          <ToolsIconContainer name='Haml'>
-            <HamlIcon color={iconColor} />
-          </ToolsIconContainer>
-          <ToolsIconContainer name='jQuery'>
-            <JQueryIcon color={iconColor} />
-          </ToolsIconContainer>
-        </ToolsIconsSection>
-
-        <ToolsIconsSection name='Libraries/Frameworks'>
-          <ToolsIconContainer name='Ruby on Rails'>
-            <RailsIcon color={iconColor} />
-          </ToolsIconContainer>
-          <ToolsIconContainer name='React'>
-            <ReactIcon color={iconColor} />
-          </ToolsIconContainer>
-          <ToolsIconContainer name='Next.js'>
-            <NextJsIcon color={iconColor} />
-          </ToolsIconContainer>
-        </ToolsIconsSection>
-
-        <ToolsIconsSection name='Version control'>
-          <ToolsIconContainer name='Git'>
-            <GitIcon color={iconColor} />
-          </ToolsIconContainer>
-          <ToolsIconContainer name='GitHub'>
-            <GitHubIcon color={iconColor} />
-          </ToolsIconContainer>
-          <ToolsIconContainer name='Bitbucket'>
-            <BitbucketIcon color={iconColor} />
-          </ToolsIconContainer>
-        </ToolsIconsSection>
-
-        <ToolsIconsSection name='Styling'>
-          <ToolsIconContainer name='Tailwind CSS'>
-            <TailwindIcon color={iconColor} />
-          </ToolsIconContainer>
-          <ToolsIconContainer name='Bootstrap'>
-            <BootstrapIcon color={iconColor} />
-          </ToolsIconContainer>
-          <ToolsIconContainer name='Sass'>
-            <SassIcon color={iconColor} />
-          </ToolsIconContainer>
-        </ToolsIconsSection>
-
-        <ToolsIconsSection name='Hosting'>
-          <ToolsIconContainer name='Heroku'>
-            <HerokuIcon color={iconColor} />
-          </ToolsIconContainer>
-          <ToolsIconContainer name='Vercel'>
-            <VercelIcon color={iconColor} />
-          </ToolsIconContainer>
-          <ToolsIconContainer name='Netlify'>
-            <NetlifyIcon color={iconColor} />
-          </ToolsIconContainer>
-        </ToolsIconsSection>
-
-        <ToolsIconsSection name='File storage'>
-          <ToolsIconContainer name='S3'>
-            <S3Icon color={iconColor} />
-          </ToolsIconContainer>
-          <ToolsIconContainer name='Cloudinary'>
-            <CloudinaryIcon color={iconColor} />
-          </ToolsIconContainer>
-        </ToolsIconsSection>
-
-        <ToolsIconsSection name='Data'>
-          <ToolsIconContainer name='PostgreSQL'>
-            <PostgreSqlIcon color={iconColor} />
-          </ToolsIconContainer>
-        </ToolsIconsSection>
-
-        <ToolsIconsSection name='Coding'>
-          <ToolsIconContainer name='VS Code'>
-            <VSCodeIcon color={iconColor} />
-          </ToolsIconContainer>
-          <ToolsIconContainer name='Sublime Text'>
-            <SublimeIcon color={iconColor} />
-          </ToolsIconContainer>
-        </ToolsIconsSection>
-
-        <ToolsIconsSection name='Testing'>
-          <ToolsIconContainer name='RSpec'>
-            <RSpecIcon color={iconColor} />
-          </ToolsIconContainer>
-        </ToolsIconsSection>
-
-        <ToolsIconsSection name='State management'>
-          <ToolsIconContainer name='Redux Toolkit'>
-            <ReduxToolkitIcon color={iconColor} />
-          </ToolsIconContainer>
-        </ToolsIconsSection>
+        <ToolsIconsSection name={languages} />
+        <ToolsIconsSection name={librariesAndFrameworks} />
+        <ToolsIconsSection name={versionControl} />
+        <ToolsIconsSection name={styling} />
+        <ToolsIconsSection name={hosting} />
+        <ToolsIconsSection name={fileStorage} />
+        <ToolsIconsSection name={dataString} />
+        <ToolsIconsSection name={coding} />
+        <ToolsIconsSection name={testing} />
+        <ToolsIconsSection name={stateManagement} />
 
         {/* TODO - Optionally add - erb, Scss, React Transition Group */}
       </div>
