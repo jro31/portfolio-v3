b5d8d6d9cd6b34e296c733bc008f64708418f60a
Jethro Williams <jethrowilliams@outlook.com>
2022-03-16 12:48:45 +0000

    Delete useDisplaySections hook and sections state slice

--- store/index.js
+++ store/index.js
@@ -3,7 +3,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import elementIsInViewReducer from './element-is-in-view';
 import liveSectionReducer from './live-section';
 import navbarReducer from './navbar';
-import sectionsReducer from './sections';
 import windowDimensionsReducer from './window-dimensions';
 import windowScrollPositionReducer from './window-scroll-position';
 
@@ -12,7 +11,6 @@ const store = configureStore({
     elementIsInView: elementIsInViewReducer,
     liveSection: liveSectionReducer,
     navbar: navbarReducer,
-    sections: sectionsReducer,
     windowDimensions: windowDimensionsReducer,
     windowScrollPosition: windowScrollPositionReducer,
   },
