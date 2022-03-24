7d639afc1f32b1b154042893e5717f801802d4bb
Jethro Williams <jethrowilliams@outlook.com>
2022-03-24 16:21:45 +0000

    Add contact page base animations

--- components/homepage-sections/contact/Contact.js
+++ components/homepage-sections/contact/Contact.js
@@ -2,18 +2,25 @@
 
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { CSSTransition } from 'react-transition-group';
 
 import { contactFormActions } from '../../../store/contact-form';
-import { contactSection } from '../../../pages';
+import {
+  contactForm,
+  contactJethroCodesLink,
+  contactSection,
+  contactSocialMediaLinks,
+  contactTitle,
+} from '../../../pages';
 import SectionContainer from '../SectionContainer';
 import Button from '../../ui/navigation/Button';
 import Title from '../../ui/text/Title';
 import FormField from '../../ui/form/FormField';
-
 import { input, textarea } from '../../ui/form/FormField';
 import SocialMediaCard from './SocialMediaCard';
 import LoadingSpinner from '../../ui/svg/LoadingSpinner';
 import JethroCodesSection from './JethroCodesSection';
+import useElementRef from '../../../hooks/useElementRef';
 
 const successMessage = 'Email sent successfully!';
 
@@ -25,12 +32,24 @@ export const twitter = 'Twitter';
 
 const Contact = () => {
   const dispatch = useDispatch();
+  const titleHasBeenInView = useSelector(
+    state => state.elementIsInView.hasBeenInView[contactTitle]
+  );
+  const formHasBeenInView = useSelector(state => state.elementIsInView.hasBeenInView[contactForm]);
+  const jethroCodesHasBeenInView = useSelector(
+    state => state.elementIsInView.hasBeenInView[contactJethroCodesLink]
+  );
+  const socialMediaLinksHaveBeenInView = useSelector(
+    state => state.elementIsInView.hasBeenInView[contactSocialMediaLinks]
+  );
   const enteredName = useSelector(state => state.contactForm.enteredName);
   const enteredEmail = useSelector(state => state.contactForm.enteredEmail);
   const enteredMessage = useSelector(state => state.contactForm.enteredMessage);
 
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [sendStatus, setSendStatus] = useState('');
+
+  const elementRef = useElementRef();
 
   const formIsValid = () =>
     enteredName.trim().length > 0 &&
@@ -85,71 +104,117 @@ const Contact = () => {
       className='bg-gradient-to-br from-slate-800 via-black to-black text-white'
     >
       <div className='flex flex-col items-end justify-around min-h-screen-minus-mobile-nav lg:min-h-screen-minus-nav h-full w-full'>
-        <div className='w-11/12'>
-          <Title>Get in touch</Title>
+        <div ref={elementRef(contactTitle)} className='w-11/12'>
+          <CSSTransition
+            mountOnEnter
+            in={titleHasBeenInView}
+            timeout={1000}
+            classNames={{ enterActive: 'animate-fade-in' }}
+          >
+            <Title>Get in touch</Title>
+          </CSSTransition>
         </div>
         <div className='flex flex-col lg:flex-row lg:justify-between gap-10 lg:gap-0 w-11/12 pr-1/12'>
-          <div className='lg:basis-1/2 xl:basis-1/3'>
-            <div className='flex flex-col gap-3'>
-              <div>Interested in working together, or just want to say hi, drop me an email...</div>
-              <form className='flex flex-col gap-3 text-black' onSubmit={formSubmitHandler}>
-                <FormField
-                  type={input}
-                  required
-                  placeholder='Name'
-                  value={enteredName}
-                  onChange={event =>
-                    handleFormFieldChange(event.target.value, contactFormActions.setEnteredName)
-                  }
-                />
-                <FormField
-                  type={input}
-                  required
-                  placeholder='Email'
-                  value={enteredEmail}
-                  onChange={event =>
-                    handleFormFieldChange(event.target.value, contactFormActions.setEnteredEmail)
-                  }
-                />
-                <FormField
-                  type={textarea}
-                  required
-                  placeholder='Message'
-                  value={enteredMessage}
-                  onChange={event =>
-                    handleFormFieldChange(event.target.value, contactFormActions.setEnteredMessage)
-                  }
-                />
-                <div className={`${sendStatus === successMessage ? 'text-success' : 'text-error'}`}>
-                  {sendStatus}
-                </div>
-                <Button disabled={disableButton()}>
-                  {isSubmitting ? (
-                    <div className='flex justify-center'>
-                      <LoadingSpinner />
+          <div ref={elementRef(contactForm)} className='lg:basis-1/2 xl:basis-1/3'>
+            <CSSTransition
+              mountOnEnter
+              in={formHasBeenInView}
+              timeout={1500}
+              classNames={{ enterActive: 'animate-fade-in lg:animate-delayed-fade-in-1' }}
+            >
+              <div>
+                <div className='flex flex-col gap-3'>
+                  <div>
+                    Interested in working together, or just want to say hi, drop me an email...
+                  </div>
+                  <form className='flex flex-col gap-3 text-black' onSubmit={formSubmitHandler}>
+                    <FormField
+                      type={input}
+                      required
+                      placeholder='Name'
+                      value={enteredName}
+                      onChange={event =>
+                        handleFormFieldChange(event.target.value, contactFormActions.setEnteredName)
+                      }
+                    />
+                    <FormField
+                      type={input}
+                      required
+                      placeholder='Email'
+                      value={enteredEmail}
+                      onChange={event =>
+                        handleFormFieldChange(
+                          event.target.value,
+                          contactFormActions.setEnteredEmail
+                        )
+                      }
+                    />
+                    <FormField
+                      type={textarea}
+                      required
+                      placeholder='Message'
+                      value={enteredMessage}
+                      onChange={event =>
+                        handleFormFieldChange(
+                          event.target.value,
+                          contactFormActions.setEnteredMessage
+                        )
+                      }
+                    />
+                    <div
+                      className={`${sendStatus === successMessage ? 'text-success' : 'text-error'}`}
+                    >
+                      {sendStatus}
                     </div>
-                  ) : (
-                    'Send email'
-                  )}
-                </Button>
-              </form>
-            </div>
-            <JethroCodesSection className='hidden lg:flex xl:hidden' />
+                    <Button disabled={disableButton()}>
+                      {isSubmitting ? (
+                        <div className='flex justify-center'>
+                          <LoadingSpinner />
+                        </div>
+                      ) : (
+                        'Send email'
+                      )}
+                    </Button>
+                  </form>
+                </div>
+                <JethroCodesSection in={true} className='hidden lg:flex xl:hidden' />
+              </div>
+            </CSSTransition>
           </div>
-          <div className='flex flex-col gap-3 lg:basis-1/2 xl:basis-1/3 lg:order-last'>
-            <div className='text-center lg:text-right'>
-              <span className='lg:hidden'>...or find me in any of these spaces...</span>
-              <span className='hidden lg:block'>...or find me in any of these spaces!</span>
-            </div>
-            <div className='flex flex-col gap-3 items-center lg:items-end'>
-              <SocialMediaCard platform={gitHub} />
-              <SocialMediaCard platform={linkedIn} />
-              <SocialMediaCard platform={instagram} />
-              <SocialMediaCard platform={twitter} />
-              <SocialMediaCard platform={email} />
-            </div>
+          <div
+            ref={elementRef(contactSocialMediaLinks)}
+            className='flex flex-col gap-3 lg:basis-1/2 xl:basis-1/3 lg:order-last'
+          >
+            <CSSTransition
+              mountOnEnter
+              in={socialMediaLinksHaveBeenInView}
+              timeout={2500}
+              classNames={{
+                enterActive:
+                  'animate-fade-in lg:animate-delayed-fade-in-2 xl:animate-delayed-fade-in-3',
+              }}
+            >
+              <div>
+                <div className='text-center lg:text-right'>
+                  <span className='lg:hidden'>...or find me in any of these spaces...</span>
+                  <span className='hidden lg:block'>...or find me in any of these spaces!</span>
+                </div>
+                <div className='flex flex-col gap-3 items-center lg:items-end'>
+                  {/* TODO - Fade these in sequentially? */}
+                  <SocialMediaCard platform={gitHub} />
+                  <SocialMediaCard platform={linkedIn} />
+                  <SocialMediaCard platform={instagram} />
+                  <SocialMediaCard platform={twitter} />
+                  <SocialMediaCard platform={email} />
+                </div>
+              </div>
+            </CSSTransition>
           </div>
-          <JethroCodesSection className='flex lg:hidden xl:flex' />
+          <JethroCodesSection
+            in={jethroCodesHasBeenInView}
+            jethroCodesRef={elementRef(contactJethroCodesLink)}
+            className='flex lg:hidden xl:flex'
+          />
         </div>
       </div>
     </SectionContainer>
--- components/homepage-sections/contact/JethroCodesSection.js
+++ components/homepage-sections/contact/JethroCodesSection.js
@@ -1,15 +1,27 @@
+import { CSSTransition } from 'react-transition-group';
+
 const JethroCodesSection = props => {
   return (
-    <div className={`flex-col xl:basis-1/3 ${props.className || ''}`}>
-      <div className='text-center'>
-        <span className='lg:hidden'>
-          ...or checkout my projects in more detail at jethro.codes!
-        </span>
-        <span className='hidden lg:block'>
-          ...or checkout my projects in more detail at jethro.codes...
-        </span>
-      </div>
-      {/* TODO - Add link */}
+    <div
+      ref={props.jethroCodesRef || null}
+      className={`flex-col xl:basis-1/3 ${props.className || ''}`}
+    >
+      <CSSTransition
+        mountOnEnter
+        in={props.in}
+        timeout={2000}
+        classNames={{ enterActive: 'animate-fade-in lg:animate-none xl:animate-delayed-fade-in-2' }}
+      >
+        <div className='text-center'>
+          <span className='lg:hidden'>
+            ...or checkout my projects in more detail at jethro.codes!
+          </span>
+          <span className='hidden lg:block'>
+            ...or checkout my projects in more detail at jethro.codes...
+          </span>
+        </div>
+        {/* TODO - Add link */}
+      </CSSTransition>
     </div>
   );
 };
