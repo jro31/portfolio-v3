553805eecd2e8e30e6a991b56cb1c6fdcde609dd
Jethro Williams <jethrowilliams@outlook.com>
2022-03-30 16:43:45 +0100

    Make classes slightly more 'Tailwindy'

--- components/ui/navigation/Hamburger.js
+++ components/ui/navigation/Hamburger.js
@@ -15,7 +15,7 @@ const Hamburger = props => {
       {[...Array(3)].map((_, i) => (
         <div
           key={`hamburger-line-${i + 1}`}
-          className={`hamburger-transitions w-[33px] h-[4px] mb-[5px] relative rounded-[3px] origin-[6px_0px] last:origin-[0%_100%] ${backgroundColorClass()} ${
+          className={`hamburger-transitions w-[33px] h-1 mb-[5px] relative rounded origin-[6px_0px] last:origin-bottom-left ${backgroundColorClass()} ${
             props.isOpen
               ? 'opacity-100 rotate-45 -translate-x-[2px] -translate-y-px even:opacity-0 even:rotate-0 even:scale-[0.2] last:-rotate-45 last:translate-x-0'
               : ''
