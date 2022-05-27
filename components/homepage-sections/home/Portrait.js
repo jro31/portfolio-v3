342591c2de9b613cc227460fdddec559e67030bc
Jethro Williams <jethrowilliams@outlook.com>
2022-05-27 18:45:20 +0100

    Add colours

--- components/homepage-sections/home/Portrait.js
+++ components/homepage-sections/home/Portrait.js
@@ -5,13 +5,27 @@ import randomElement from 'random-element-selector';
 
 import { homePortrait } from '../../../pages';
 
-const backgroundColorClasses = ['from-blue-500 via-blue-900', 'from-emerald-500 via-emerald-900'];
+const backgroundColorClasses = [
+  'from-blue-500 via-blue-900',
+  'from-violet-500 via-violet-900',
+  'from-purple-500 via-purple-900',
+  'from-indigo-500 via-indigo-900',
+  'from-sky-500 via-sky-900',
+  'from-yellow-500 via-yellow-900',
+  'from-amber-500 via-amber-900',
+  'from-orange-500 via-orange-900',
+];
 
 const backgroundBeforeColorClasses = [
   'before:from-green-500 before:via-green-900',
+  'before:from-emerald-500 before:via-emerald-900',
   'before:from-rose-500 before:via-rose-900',
   'before:from-pink-500 before:via-pink-900',
   'before:from-fuchsia-500 before:via-fuchsia-900',
+  'before:from-cyan-500 before:via-cyan-900',
+  'before:from-teal-500 before:via-teal-900',
+  'before:from-lime-500 before:via-lime-900',
+  'before:from-red-500 before:via-red-900',
 ];
 
 const Portrait = () => {
@@ -22,7 +36,7 @@ const Portrait = () => {
     'from-blue-500 via-blue-900'
   );
   const [currentBackgroundBeforeColorClasses, setCurrentBackgroundBeforeColorClasses] = useState(
-    'before:from-fuchsia-500 before:via-fuchsia-900'
+    'before:from-emerald-500 before:via-emerald-900'
   );
   const [backgroundBeforeIsTransparent, setBackgroundBeforeIsTransparent] = useState(true);
   const backgroundBeforeIsTransparentRef = useRef(backgroundBeforeIsTransparent);
